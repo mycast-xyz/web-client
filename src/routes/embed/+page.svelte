3bbeec4c3ad0a0e2bd2dd0eef5d31db0d468c743
엘풍 <eruhoon@gmail.com>
2023-10-28 10:27:14 +0900

    update embed route

---
+++ src/routes/embed/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import type { LocalStreamSource } from '../../app/model/window/Content';
+	import LocalStreamContentView from '../../app/view/main/content/LocalStreamContentView.svelte';
+	import TotoroStreamContentView from '../../app/view/main/content/TotoroStreamContentView.svelte';
+
+	const urlParams = $page.url.searchParams;
+	const hasId = urlParams.has('id');
+	const id = urlParams.get('id') ?? '';
+	const platform = urlParams.get('p');
+
+	let src: LocalStreamSource = {
+		icon: '',
+		url: id,
+		title: ''
+	};
+</script>
+
+{#if hasId}
+	{#if platform === 'local'}
+		<LocalStreamContentView {src} />
+	{:else if platform === 'totoro'}
+		<TotoroStreamContentView {src} />
+		totoro
+	{:else}
+		<p>error</p>
+	{/if}
+{:else}
+	<p>error</p>
+{/if}
