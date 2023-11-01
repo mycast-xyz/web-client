ff5da9d7c16fe9fb96641ad1e60c01f1b52bc448
엘풍 <eruhoon@gmail.com>
2023-11-01 22:12:10 +0900

    fix bug in Embedpage

--- src/routes/embed/+page.svelte
+++ src/routes/embed/+page.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import { onMount } from 'svelte';
 
   import type { LocalStreamSource } from '../../app/model/window/Content';
   import LocalStreamContentView from '../../app/view/main/content/LocalStreamContentView.svelte';
   import TotoroStreamContentView from '../../app/view/main/content/TotoroStreamContentView.svelte';
 
-  const urlParams = $page.url.searchParams;
-  const hasId = urlParams.has('id');
-  const id = urlParams.get('id') ?? '';
-  const platform = urlParams.get('p');
+  let hasId: boolean = false;
+  let id: string = '';
+  let platform: string = 'local';
 
   let src: LocalStreamSource = {
     icon: '',
     url: id,
     title: ''
   };
+
+  onMount(() => {
+    let urlParams = $page.url.searchParams;
+    hasId = urlParams.has('id');
+    id = urlParams.get('id') ?? '';
+    platform = urlParams.get('p') ?? 'local';
+  });
 </script>
 
 {#if hasId}
