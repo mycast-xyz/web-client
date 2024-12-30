359ba46fe3a1c99cc79c190e593a40ff7167c8d5
peng_jo <85087860+peng-jo@users.noreply.github.com>
2024-12-30 14:09:58 +0900

    update HappyNewYearEventItem.svelte

--- src/app/view/main/bottom/HappyNewYearEventItem.svelte
+++ src/app/view/main/bottom/HappyNewYearEventItem.svelte
@@ -4,7 +4,7 @@
   import BottomBarItem from './BottomBarItem.svelte';
 
   function onIconClick() {
-    WindowService.openImageViewerPopup('https://i.ibb.co/mq56D7k/thumb.jpg');
+    WindowService.openImageViewerPopup('https://i.imgur.com/3W7Pwao.png');
   }
 </script>
 
